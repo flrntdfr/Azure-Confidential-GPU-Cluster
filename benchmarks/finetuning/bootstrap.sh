f49628ceb962dea7f18adbf4feddbf5c2a851a65
Florent Dufour <florent@lrz.de>
2025-10-20 15:23:54 +0200

    Independent bootstraps

--- benchmarks/finetuning/bootstrap.sh
+++ benchmarks/finetuning/bootstrap.sh
@@ -1,7 +1,36 @@
+#!/bin/bash
+# Bootstrap environment for finetuning benchmarks
+
+# uv
+if ! command -v uv >/dev/null 2>&1; then
+  curl -LsSf https://astral.sh/uv/install.sh | sh > /dev/null 2>&1
+fi
+
+# medAlpaca
 git clone --depth=1 --branch reproducible https://github.com/flrntdfr/medAlpaca.git
 cd medAlpaca
 uv venv --prompt medAlapaca --python 3.10.8
 source .venv/bin/activate
 uv pip install -r requirements.txt
+
+# Get credentials from .env file
+set -a
+source "../env"
+set +a
+
+#export HF_HOME=$HOME/.cache/huggingface FIXME
+export HF_HOME=/dss/dsshome1/lxc00/di67pif/dssfs04/.cache/huggingface
+
+# HuggingFace
+## For now
+export HF_TOKEN="${HF_TOKEN}"
+## For later
+hf auth login --token "${HF_TOKEN}"
+
+# Weights and biases
+## For now
+export WANDB_API_KEY="${WANDB_API_KEY}"
+## For later
+echo "${WANDB_TOKEN}" | wandb login --relogin
 
 hf download meta-llama/Llama-2-7b-hf
--- benchmarks/inference/bootstrap.sh
+++ benchmarks/inference/bootstrap.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 # Bootstrap environment for inference benchmarks
 
-set -euo pipefail
+# uv
+if ! command -v uv >/dev/null 2>&1; then
+  curl -LsSf https://astral.sh/uv/install.sh | sh > /dev/null 2>&1
+fi
 
+# vLLM
 uv venv --prompt vllm --python 3.12
 source .venv/bin/activate
 uv pip install vllm==0.11.0 huggingface-hub==0.35.3 tqdm==4.67.1
 
-export HF_HOME=$HOME/.cache/huggingface
+# Get credentials from .env file
+set -a
+source "../env"
+set +a
 
-# FIXME
-for model in \ 
+#export HF_HOME=$HOME/.cache/huggingface FIXME
+export HF_HOME=/dss/dsshome1/lxc00/di67pif/dssfs04/.cache/huggingface
+
+# HuggingFace
+## For now
+export HF_TOKEN="${HF_TOKEN}"
+## For later
+hf auth login --token "${HF_TOKEN}"
+
+for model in \
+    google/gemma-3-1b-it \
     meta-llama/Llama-3.1-8B-Instruct \
     Qwen/Qwen3-32B \
     deepseek-ai/DeepSeek-R1-Distill-Llama-70B
