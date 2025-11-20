ebc9f551763dc3e453a5e2e9b8097b3a8613f6db
Florent Dufour <florent@lrz.de>
2025-11-20 10:33:15 +0100

    BitFit implemented

--- benchmarks/finetuning/src/arguments.py
+++ benchmarks/finetuning/src/arguments.py
@@ -52,6 +52,53 @@ class ModelArguments:
     )
 
 @dataclass
+class PrivacyArguments:
+    enable_dp: bool = field(
+        default=False,
+        metadata={"help": "Whether to enable Differential Privacy using fastDP."}
+    )
+    target_epsilon: float = field(
+        default=2.0,
+        metadata={"help": "Target privacy budget ε."}
+    )
+    target_delta: Optional[float] = field(
+        default=1e-5,
+        metadata={"help": "Target privacy budget δ, should be smaller than 1/sample_size."}
+    )
+    per_sample_max_grad_norm: float = field(
+        default=1.0,
+        metadata={"help": "Per-sample gradient clipping threshold, default to 1. No need to tune if clipping_fn='automatic'."}
+    )
+    noise_multiplier: Optional[float] = field(
+        default=None,
+        metadata={"help": "Level of independent Gaussian noise into the gradient. This can be automatically computed by different accounting_mode if target_epsilon, batch_size, sample_size, epochs are provided."}
+    )
+    accounting_mode: str = field(
+        default="rdp",
+        metadata={"help": "Privacy accounting theory to use, one of 'rdp' (default), 'glw', 'all'."}
+    )
+    named_params: Optional[List[str]] = field(
+        default=None,
+        metadata={"help": "Specifies which parameters to optimize with differential privacy."}
+    )
+    clipping_mode: str = field(
+        default="MixOpt",
+        metadata={"help": "Per-sample gradient clipping mode, one of 'ghost', 'MixGhostClip', 'MixOpt' (default) from [4]. Note different clipping modes, including Opacus [5], GhostClip [2] and Mixed GhostClip [3], give the same convergence and accuracy though at significantly different time/space complexity."}
+    )
+    clipping_fn: str = field(
+        default="automatic",
+        metadata={"help": "Per-sample gradient clipping function to use; one of 'automatic' (default, Bu et al., 2022), 'Abadi' (Abadi et al., 2016) , 'global' (Bu et al., 2021)."}
+    )
+    clipping_style: str = field(
+        default="all-layer",
+        metadata={"help": "Per-sample gradient clipping style to use; one of all-layer (flat clipping), layer-wise (each layer is a block, including both weight and bias parameters), param-wise (each parameter is a block), or a list of layer names (general block-wise clipping)."}
+    )
+    origin_params: Optional[List[str]] = field(
+        default=None,
+        metadata={"help": "Origin parameters for the ghost differentiation trick from Bu et al. Appendix D.3. Default is None (not using the trick). To enjoy the acceleration from the trick, set to each model's first trainable layer's parameters. For example, in text classification with RoBERTa, set origin_params=['_embeddings']; in text generation with GPT2, set origin_params=['wte','wpe']; in image classification with BEiT, set origin_params=['patch_embed.proj.bias']. This trick gives about 8/6=1.666 speedup at no memory overhead."}
+    )
+
+@dataclass
 class TrainingArguments(SFTConfig):
     seed: int = field(
         default=54940,
@@ -109,37 +156,11 @@ class TrainingArguments(SFTConfig):
         default=True, 
         metadata={"help": "Whether or not to group together samples of roughly the same length in the training dataset."}
     )
+    save_strategy: str = field(
+        default="no",
+        metadata={"help": "The checkpoint save strategy to use."}
+    )
 
-# privacy_engine = PrivacyEngine(
-#     model,
-#     batch_size=256,
-#     sample_size=50000,
-#     epochs=3,
-#     target_epsilon=2,
-#     clipping_fn='automatic',
-#     clipping_mode='MixOpt',
-#     origin_params=None,
-#     clipping_style='all-layer',
-# )
-
-@dataclass # FIXME
-class PrivacyArguments:
-    enable_dp: bool = field(
-        default=False,
-        metadata={"help": "Whether to enable Differential Privacy using fastDP."}
-    )
-    target_epsilon: float = field(
-        default=8.0,
-        metadata={"help": "The target epsilon for differential privacy."}
-    )
-    target_delta: float = field(
-        default=1e-5,
-        metadata={"help": "The target delta for differential privacy."}
-    )
-    max_grad_norm: float = field(
-        default=1.0,
-        metadata={"help": "Maximum gradient norm for clipping in DP."}
-    )
 
 @dataclass
 class WandbArguments:
--- benchmarks/finetuning/src/train.py
+++ benchmarks/finetuning/src/train.py
@@ -9,39 +9,34 @@ from arguments import (
     AdapterArguments,
     DataArguments,
     ModelArguments,
+    PrivacyArguments,
     TrainingArguments,
-    WandbArguments
+    WandbArguments,
 )
-from datasets import (
-    load_dataset
-)
-from peft import (
-    LoraConfig,
-    get_peft_model,
-    prepare_model_for_kbit_training
-)
+from datasets import load_dataset
+from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training
 from transformers import (
     AutoModelForCausalLM,
     AutoTokenizer,
     BitsAndBytesConfig,
     HfArgumentParser,
+    set_seed,
 )
-from trl import (
-    SFTTrainer
-)
+from trl import SFTTrainer
 
 logger = logging.getLogger(__name__)
 
-def setup_logging(training_args, adapter_args, wandb_args):
+def setup_logging(adapter_args, privacy_args, training_args, wandb_args):
     logging.basicConfig(
         format="%(asctime)s %(levelname)s - %(name)s: %(message)s",
         datefmt="%Z-%m-%d %H:%M:%S",
-        handlers=[logging.StreamHandler(sys.stdout)]
+        handlers=[logging.StreamHandler(sys.stdout)],
+        force=True,
     )
-    
-    log_level = training_args.get_process_log_level()
+
+    log_level = logging.DEBUG
     logger.setLevel(log_level)
-    
+
     if training_args.should_log:
         transformers.utils.logging.set_verbosity_info()
     transformers.utils.logging.set_verbosity(log_level)
@@ -50,8 +45,8 @@ def setup_logging(training_args, adapter_args, wandb_args):
     logger.info(f"Logging {__name__}")
 
     if wandb_args.use_wandb:
-
         tags_list = []
+        notes = ""
         if adapter_args.use_lora:
             tags_list.append("lora")
         if adapter_args.load_in_4bit:
@@ -63,22 +58,41 @@ def setup_logging(training_args, adapter_args, wandb_args):
         if training_args.fp16:
             tags_list.append("fp16")
         tags_list.append(f"pbs-{training_args.per_device_train_batch_size}")
+        if privacy_args.enable_dp:
+            tags_list.append("BitFit")
+            tags_list.append("ε-" + str(privacy_args.target_epsilon))
+            notes += "DP settings:"
+            notes += f"epsilon: {privacy_args.target_epsilon}"
+            notes += f"delta: {privacy_args.target_delta}"
+            notes += f"max_grad_norm: {privacy_args.per_sample_max_grad_norm}"
+            notes += f"noise_multiplier: {privacy_args.noise_multiplier}"
+            notes += f"accounting_mode: {privacy_args.accounting_mode}"
+            notes += f"clipping_mode: {privacy_args.clipping_mode}"
+            notes += f"clipping_fn: {privacy_args.clipping_fn}"
+            notes += f"clipping_style: {privacy_args.clipping_style}"
+            notes += f"origin_params: {privacy_args.origin_params}"
+            notes += f"named_params: {privacy_args.named_params}"
         if wandb_args.wandb_tags:
-            tags_list.extend(wandb_args.wandb_tags.split(","))
-
+            tags_list.extend([str(tag) for tag in wandb_args.wandb_tags.split(",")])
+        if wandb_args.wandb_notes:
+            notes += wandb_args.wandb_notes
+        
         wandb.init(
             project=wandb_args.wandb_project,
             name=wandb_args.wandb_run_name,
             tags=tags_list,
-            notes=wandb_args.wandb_notes
+            notes=notes,
         )
 
         training_args.report_to = ["wandb"]
         training_args.run_name = wandb_args.wandb_run_name
     else:
-        training_args.report_to = [] # Explicitly disable WANDB reporting
-    
-    logger.info(training_args)
+        training_args.report_to = []  # Explicitly disable WANDB reporting
+
+    logger.debug(adapter_args)
+    logger.debug(privacy_args)
+    logger.debug(training_args)
+
 
 def get_model(model_args, adapter_args, training_args):
     dtype = torch.float32
@@ -89,7 +103,7 @@ def get_model(model_args, adapter_args, training_args):
 
     if adapter_args.load_in_4bit and adapter_args.load_in_8bit:
         raise ValueError("Choose either 4bit _or_ 8bit")
-    
+
     bnb_config = None
     if adapter_args.load_in_4bit:
         bnb_config = BitsAndBytesConfig(
@@ -100,11 +114,9 @@ def get_model(model_args, adapter_args, training_args):
         )
         logger.info("Will load model in 4bit")
     elif adapter_args.load_in_8bit:
-        bnb_config = BitsAndBytesConfig(
-            load_in_8bit=True
-        )
+        bnb_config = BitsAndBytesConfig(load_in_8bit=True)
         logger.info("Will load model in 8bit")
-    
+
     # Model
     logger.info(f"Loading {model_args.model}")
     model = AutoModelForCausalLM.from_pretrained(
@@ -112,7 +124,7 @@ def get_model(model_args, adapter_args, training_args):
         quantization_config=bnb_config,
         device_map="auto" if not os.environ.get("LOCAL_RANK") else None,
         attn_implementation="flash_attention_2" if model_args.use_flash_attn else None,
-        dtype=dtype
+        dtype=dtype,
     )
 
     # Quantization
@@ -128,14 +140,57 @@ def get_model(model_args, adapter_args, training_args):
             lora_dropout=adapter_args.lora_dropout,
             bias="none",
             task_type="CAUSAL_LM",
-            target_modules=adapter_args.lora_target_modules
+            target_modules=adapter_args.lora_target_modules,
         )
         model = get_peft_model(model, peft_config)
         model.print_trainable_parameters()
 
     return model, peft_config if adapter_args.use_lora else None
 
-# TODO add DPTrainer
+
+class DPTrainer(SFTTrainer):
+    def __init__(self, privacy_args, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.privacy_args = privacy_args
+        assert self.privacy_args.target_delta < 1 / len(self.train_dataset), "Target privacy budget δ, should be smaller than 1/sample_size."
+
+    def create_optimizer(self):
+        """Wrap the optimizer with the PrivacyEngine"""
+        from fastDP import PrivacyEngine
+
+        optimizer = super().create_optimizer()
+        logger.info("Wrapping optimizer with fastDP PrivacyEngine")
+        self.privacy_engine = PrivacyEngine(
+            self.model,
+            batch_size=self.args.per_device_train_batch_size * self.args.gradient_accumulation_steps,
+            sample_size=len(self.train_dataset),
+            epochs=self.args.num_train_epochs,
+            target_epsilon=self.privacy_args.target_epsilon,
+            target_delta=self.privacy_args.target_delta,
+            max_grad_norm=self.privacy_args.per_sample_max_grad_norm,
+            noise_multiplier=self.privacy_args.noise_multiplier,
+            accounting_mode=self.privacy_args.accounting_mode,
+            clipping_mode=self.privacy_args.clipping_mode,
+            clipping_fn=self.privacy_args.clipping_fn,
+            clipping_style=self.privacy_args.clipping_style,
+            origin_params=self.privacy_args.origin_params,
+            named_params=self.privacy_args.named_params,
+        )
+        self.privacy_engine.attach(optimizer)
+
+        # ----- BEGIN HACK ---- #
+        import types
+        original_step = optimizer.step
+
+        def step_with_positional_closure(_self, closure=None, **kwargs):
+            return original_step(closure=closure, **kwargs)
+        
+        optimizer.step = types.MethodType(step_with_positional_closure, optimizer)
+        # ----- END HACK -----#
+
+        logger.info("Privacy engine attached!")
+        return optimizer
+
 
 def formatting_prompts_func(example):
     instruction = example.get("instruction", "")
@@ -144,21 +199,33 @@ def formatting_prompts_func(example):
     prompt = f"{instruction}\n: {input}"
     text = f"{prompt} {output}"
     return text
-        
-def main():
-    parser = HfArgumentParser((ModelArguments, DataArguments, AdapterArguments, WandbArguments, TrainingArguments))
-    model_args, data_args, adapter_args, wandb_args, training_args = parser.parse_args_into_dataclasses()
 
-    setup_logging(training_args, adapter_args, wandb_args)
+
+def main():
+    parser = HfArgumentParser(
+        (
+            AdapterArguments,
+            DataArguments,
+            ModelArguments,
+            PrivacyArguments,
+            TrainingArguments,
+            WandbArguments,
+        )
+    )
+    adapter_args, data_args, model_args, privacy_args, training_args, wandb_args = (
+        parser.parse_args_into_dataclasses()
+    )
+
+    set_seed(training_args.seed)
+
+    setup_logging(adapter_args, privacy_args, training_args, wandb_args)
 
     # Load model
     model, peft_config = get_model(model_args, adapter_args, training_args)
 
     # Load tokenizer
     tokenizer = AutoTokenizer.from_pretrained(
-        model_args.model,
-        padding_side="right",
-        use_fast=True
+        model_args.model, padding_side="right", use_fast=True
     )
     tokenizer.pad_token = tokenizer.eos_token
 
@@ -166,29 +233,37 @@ def main():
     logger.info(f"Loading dataset: {data_args.dataset_path}")
     dataset = load_dataset("json", data_files=data_args.dataset_path)
 
+    TrainerClass = DPTrainer if privacy_args.enable_dp else SFTTrainer
+
     # Init the trainer
-    trainer = SFTTrainer(
-        model=model,
-        train_dataset=dataset["train"],
-        eval_dataset=dataset["test"] if "test" in dataset else None,
-        peft_config=peft_config,
-        processing_class=tokenizer,
-        args=training_args,
-        formatting_func=formatting_prompts_func,
-    )
+    trainer_kwargs = {
+        "model": model,
+        "train_dataset": dataset["train"],
+        "eval_dataset": dataset["test"] if "test" in dataset else None,
+        "peft_config": peft_config,
+        "processing_class": tokenizer,
+        "args": training_args,
+        "formatting_func": formatting_prompts_func,
+    }
+
+    if privacy_args.enable_dp:
+        trainer_kwargs["privacy_args"] = privacy_args
+
+    trainer = TrainerClass(**trainer_kwargs)
 
     # Train
     logger.info("Starting training")
     train_result = trainer.train()
 
     # Save model or not
-    if training_args.should_save: # TODO check if all checkpoints are disbaled
+    if training_args.save_strategy != "no":
         trainer.save_model()
-    
+
     # Metrics
     metrics = train_result.metrics
     trainer.log_metrics("train", metrics)
     trainer.save_metrics("train", metrics)
+
 
 if __name__ == "__main__":
     main()
